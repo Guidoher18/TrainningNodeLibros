0813ecf7bd3fb09c490a2680c6fcdbf0e4caf781
Guido Caruso <guido.caruso.ext@errepar.com>
2025-03-31 13:10:19 -0300

    Add Routes.ts with new EP:  /api/saludo

---
+++ src/routes/routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/saludo", (req, res) => {
+    res.json({saludo: "Hola desde otra ruta!"});
+});
+
+export default router;
