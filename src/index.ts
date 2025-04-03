21aa3f0965add4f822566511d10f6b8bb39adb02
Guido Caruso <guido.caruso.ext@errepar.com>
2025-04-03 08:51:17 -0300

    Fix format and style with prettier

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
 import express from 'express';
 import router from './routes/routes';
+import dotenv from 'dotenv';
 
 const app = express();
 
+dotenv.config();
+
 const PORT = process.env.PORT ?? 3000;
 
-app.disable("x-powered-by");
+app.disable('x-powered-by');
 
 // Middleware para parsear json
-app.use(express.json())
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hola Mundo!</h1>");
+app.get('/', (req, res) => {
+  res.send('<h1>Hola Mundo!</h1>');
 });
 
-app.use("/api", router);
+app.use('/api', router);
 
 app.listen(PORT, () => {
-    console.log(`App running on http://localhost:${PORT}`);
-}); 
+  console.log(`App running on http://localhost:${PORT}`);
+});
